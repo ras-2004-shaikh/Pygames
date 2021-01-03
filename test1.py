5fe2e0c33725f5dd2bccb58a2405b6fe644249d4
ras-2004-shaikh <76616693+ras-2004-shaikh@users.noreply.github.com>
2021-01-03 21:00:55 +0530

    Add files via upload
    A physics bouncy

---
+++ test1.py
@@ -0,0 +1,51 @@
+import pygame
+
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Second Game")
+
+run=True
+x=50
+y=450
+r=10
+ax=0
+ay=1
+vx=0
+vy=0
+rx=0
+ry=0
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+        if event.type == pygame.KEYDOWN:
+            k2 = pygame.key.get_pressed()
+            if k2[pygame.K_UP]:
+                vy -= 10
+            elif k2[pygame.K_DOWN]:
+                if vy<0:vy=0
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        ax=-3
+    if keys[pygame.K_RIGHT]:
+        ax=+3
+    if keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]:
+        ax=0
+    if (not keys[pygame.K_LEFT]) and (not keys[pygame.K_RIGHT]):
+        ax=0
+    if keys[pygame.K_b]:
+        ax = -vx/3
+    rx=-vx/50
+    ry=-vy/50
+    vx+=ax + rx
+    vy+=ay + ry
+    x+=vx
+    y+=vy
+    if y>=490 and vy > 0:
+        y=490
+        vy=-vy/2
+    win.fill((0,0,0))
+    pygame.draw.circle(win,(255,0,0), (x,y),r)
+    print(x,y)
+    pygame.display.update()
+pygame.quit()
