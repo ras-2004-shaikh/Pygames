b621eba026f0ee3c285b1fda2bf4aa6ae9039d72
ras-2004-shaikh <76616693+ras-2004-shaikh@users.noreply.github.com>
2021-01-04 11:08:49 +0530

    Add files via upload
    Physics bouncy take 2. A little less responsive. A little more continuous. Same as before just a few lines here and there. Without rx and ry almost 100% energy conservation with bounces.

--- test1.py
+++ test1.py
@@ -2,26 +2,36 @@ import pygame
 
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Second Game")
-
+pygame.init()
 run=True
 x=50
 y=450
 r=10
 ax=0
-ay=1
+ay=0.001
 vx=0
 vy=0
 rx=0
 ry=0
+e=1
+m=0
+p=pygame.time.get_ticks()
+print(p)
+pygame.time.delay(100)
+print(pygame.time.get_ticks())
 while run:
-    pygame.time.delay(75)
+    m=pygame.time.get_ticks()
+    dt=m-p
+    p=pygame.time.get_ticks()
+    print(dt)
+    pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
         if event.type == pygame.KEYDOWN:
             k2 = pygame.key.get_pressed()
             if k2[pygame.K_UP]:
-                vy -= 10
+                vy -= 0.5
             elif k2[pygame.K_DOWN]:
                 if vy<0:vy=0
             if k2[pygame.K_SPACE]:
@@ -29,32 +39,32 @@ while run:
                 vx*=1.05
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        ax=-3
+        ax=-0.005
     if keys[pygame.K_RIGHT]:
-        ax=+3
+        ax=+0.005
     if keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]:
         ax=0
     if (not keys[pygame.K_LEFT]) and (not keys[pygame.K_RIGHT]):
         ax=0
     if keys[pygame.K_b]:
-        ax = -vx/3
-    rx=-vx/50
-    ry=-vy/50
-    vx+=ax #+ rx
-    vy+=ay #+ ry
-    x+=vx
-    y+=vy
+        ax = -vx/300
+    rx=-vx/500
+    ry=-vy/500
+    vx+=ax*dt #+ rx*dt
+    vy+=ay*dt #+ ry*dt
+    x+=vx*dt
+    y+=vy*dt
     if y>=490 and vy > 0:
         y=490
-        vy=-vy
+        vy=-vy*e
     if y<=10:
-        vy=-vy
+        vy=-vy*e
         y=10
     if x<=10:
-        vx=-vx
+        vx=-vx*e
         x=10
     if x>=490:
-        vx=-vx
+        vx=-vx*e
         x = 490
     win.fill((0,0,0))
     pygame.draw.circle(win,(255,0,0), (x,y),r)
