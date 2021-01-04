9d551d0b0eb4233c5fce3e74cf00ca02646072cc
ras-2004-shaikh <76616693+ras-2004-shaikh@users.noreply.github.com>
2021-01-04 21:17:35 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,25 +5,25 @@ win = pygame.display.set_mode((wd,he))
 pygame.display.set_caption("Second Game")
 pygame.init()
 run=True
-r=5
+radius=5
 ax=0
 ay=0.001
 vx=0
 vy=0
-rx=0
-ry=0
-e=1
-m=0
-x=r+50
-y=he-r-50
-p=pygame.time.get_ticks()
-print(p)
+resistX=0
+resistY=0
+elasticity=1
+new_time=0
+x=radius+50
+y=he-radius-50
+time=pygame.time.get_ticks()
+print(time)
 pygame.time.delay(100)
 print(pygame.time.get_ticks())
 while run:
-    m=pygame.time.get_ticks()
-    dt=m-p
-    p=pygame.time.get_ticks()
+    new_time=pygame.time.get_ticks()
+    dt=new_time-time
+    time=pygame.time.get_ticks()
     pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -48,26 +48,26 @@ while run:
         ax=0
     if keys[pygame.K_b]:
         ax = -vx/300
-    rx=-vx/600
-    ry=-vy/600
-    vx+=ax*dt #+ rx*dt
-    vy+=ay*dt #+ ry*dt
+    resistX=-vx/600
+    resistY=-vy/600
+    vx+=ax*dt #+ resistX*dt
+    vy+=ay*dt #+ resistY*dt
     x+=vx*dt
     y+=vy*dt
-    if y>=he-r and vy > 0:
-        y=he-r
-        vy=-vy*e
-    if y<=r:
-        vy=-vy*e
-        y=r
-    if x<=r:
-        vx=-vx*e
-        x=r
-    if x>=wd-r:
-        vx=-vx*e
-        x = wd-r
+    if y>=he-radius and vy > 0:
+        y=he-radius
+        vy=-vy*elasticity
+    if y<=radius:
+        vy=-vy*elasticity
+        y=radius
+    if x<=radius:
+        vx=-vx*elasticity
+        x=radius
+    if x>=wd-radius:
+        vx=-vx*elasticity
+        x = wd-radius
     win.fill((0,0,0))
-    pygame.draw.circle(win,(255,0,0), (x,y),r)
+    pygame.draw.circle(win,(255,0,0), (x,y),radius)
     print(x,y)
     pygame.display.update()
 pygame.quit()
