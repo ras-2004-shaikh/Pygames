271ecaa8714a68894205a7e90ff10cbfc548e084
ras-2004-shaikh <76616693+ras-2004-shaikh@users.noreply.github.com>
2021-01-09 13:09:10 +0530

    Add files via upload
    My first official Hello World (kinda) into pygame.

--- Flappy main.py
+++ Flappy main.py
@@ -2,11 +2,8 @@ import pygame
 import os
 import neat
 import random
-pygame.init()
-game_started=False
 start=pygame.time.get_ticks()
-count1=0
-count2=0
+pygame.init()
 class Physicsworld:
     def __init__(self,gravity,elasticity,resistance,normal,brake_resistance,push,resistance_present=True):
         self.gravity=gravity
@@ -23,6 +20,8 @@ class Bird:
     ROT_VEL=100
     def __init__(self,pos,size,response,world,keys=(pygame.K_SPACE,0)):
         self.tag="player"
+        self.game_started = False
+        self.screen=None
         self.pos={'x':pos[0],'y':pos[1]}
         self.size = {'x': size[0], 'y': size[1]}
         self.response=response
@@ -33,6 +32,9 @@ class Bird:
         self.anime_count=0
         self.tilt=0
         self.img=self.IMGS[0]
+        self.score=0
+        self.killed=False
+        self.just_killed=False
     def redraw(self,screen):
         self.img=self.IMGS[int(self.anime_count/self.ANIMATION_TIME)%4]
         if self.tilt<=-60:
@@ -42,22 +44,24 @@ class Bird:
         screen.blit(rotated_img,new_rect)
         if self.anime_count/self.ANIMATION_TIME >=4:
             self.anime_count=0
-        pygame.display.update()
     def event_manager(self,event,keys):
-        global game_started
         global start
         if event.type == pygame.KEYDOWN:
             if keys[self.jump_key]:
-                if not game_started:
-                    game_started=True
+                if not self.game_started:
+                    self.game_started=True
                     start=pygame.time.get_ticks()
-                self.jump()
+                if not self.killed:
+                    self.jump()
     def general_operations_manager(self,keys,dt):
         self.move(dt)
         pass
     def move(self,dt):
-        global game_started
-        if game_started:
+        if self.just_killed:
+            self.screen.stop()
+            self.velocity=0
+            self.just_killed=False
+        if self.game_started:
             self.velocity+=(self.acceleration - self.velocity * abs(self.velocity) * self.world.resistance)*dt
             self.pos['y']+=self.velocity*dt
             if self.velocity<0:
@@ -67,11 +71,20 @@ class Bird:
                     self.tilt-=self.ROT_VEL*dt
         else:
             pass
-        self.anime_count+=dt
+        if (self.pos['y']<=0 or self.pos['y']+self.img.get_height()>=600) and not self.killed:
+            self.kill()
+        if not self.killed:
+            self.anime_count+=dt
     def jump(self):
         self.velocity=-self.response*self.world.jump_normal
+        self.pos['y']+=self.velocity*0.002
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+    def kill(self):
+        self.just_killed=True
+        self.killed=True
+    def set_screen(self,screen):
+        self.screen=screen
 
 class Screen:
     def __init__(self,pos,size,objects=[],velocity=500):
@@ -86,12 +99,15 @@ class Screen:
         self.BG_pos = [(0, 0)]
         self.objects=objects
         self.dir=0
+        self.stopped=False
     def event_manager(self,event,keys):
         pass
     def general_operations_manager(self,keys,dt):
         self.dir=-1
         self.move(dt)
     def move(self,dt):
+        if self.stopped:
+            self.scroll_vel=0
         self.scroll += self.scroll_vel * self.dir*dt
         self.scroll %= 600
         if self.scroll < 300:
@@ -101,26 +117,39 @@ class Screen:
         else:
             self.BG_pos = [(0, 0)]
     def redraw(self):
-        global count2
         self.WIN.fill((235,240,255))
         for rect in self.BG_pos:
             self.WIN.blit(self.BG, rect)
         for obj in self.objects:
             if obj.tag=="enemy" and obj.passed:
                 self.objects.remove(obj)
-            obj.redraw(self.WIN)
-        count2+=1
+            else:
+                obj.redraw(self.WIN)
+        if not self.stopped:
+            text = Font.render("Score: " + str(bird1.score), 1, (150, 150, 0))
+            self.WIN.blit(text, (5, 5))
+        else:
+            text = Font.render("GAME OVER",1,(255,0,255))
+            self.WIN.blit(text,(250,250))
+            text=Font.render(f"Score: {bird1.score}",1,(255,0,255))
+            self.WIN.blit(text,(260,290))
+        if not bird1.game_started:
+            text=Font.render("Press space to flap",1,(255,0,0))
+            self.WIN.blit(text,(200,250))
         pygame.display.update()
+    def stop(self):
+        self.stopped=True
 
 class Pipe:
-    GAP = 100
+    GAP = 150
+    PIPE=pygame.image.load("Flappy/Pipe.png")
     def __init__(self,screen,x,player):
         self.tag="enemy"
         self.x=x
         self.player = player
         self.screen=screen
-        self.PIPE_BOTTOM = pygame.image.load("Flappy/Pipe.png")
-        self.PIPE_TOP = pygame.transform.rotate(self.PIPE_BOTTOM, 180)
+        self.PIPE_BOTTOM = self.PIPE
+        self.PIPE_TOP = pygame.transform.rotate(self.PIPE, 180)
         self.vel=self.screen.scroll_vel
         self.height=0
         self.top=0
@@ -129,35 +158,51 @@ class Pipe:
         self.length=self.PIPE_TOP.get_height()
         self.passed=False
         self.set_height()
+        self.collided=False
     def set_height(self):
-        self.height = random.randrange(50,380)
+        self.height = random.randrange(100,350)
         self.top=self.height - self.PIPE_TOP.get_height()
         self.bottom=self.height+self.GAP
     def event_manager(self,event,keys):
         pass
     def general_operations_manager(self,keys,dt):
         self.move(dt)
+        if self.collide() and not self.collided:
+            self.player.kill()
+            self.collided=True
     def move(self,dt):
+        self.vel=self.screen.scroll_vel
         self.x-=self.vel*dt
-        if self.x + self.width <= self.player.pos['x']:
+        if self.x + self.width <= 0:
             self.passed=True
+            self.player.score+=5
     def redraw(self,window):
-        global count1
         window.blit(self.PIPE_TOP,(self.x,self.top,self.width,self.length))
         window.blit(self.PIPE_BOTTOM,(self.x,self.bottom,self.width,self.length))
-        count1+=1
+    def collide(self):
+        bird_mask=self.player.get_mask()
+        top_mask=pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask=pygame.mask.from_surface(self.PIPE_BOTTOM)
+        top_offset=(round(self.x-self.player.pos['x']),round(self.top-self.player.pos['y']))
+        bottom_offset=(round(self.x-self.player.pos['x']),round(self.bottom-self.player.pos['y']))
+        b_point = bird_mask.overlap(bottom_mask,bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+        return t_point or b_point
+
 
 #Main loop
 run=True
 clock=pygame.time.Clock()
-world=Physicsworld(gravity=1200,elasticity=1,resistance=0.00390625,normal=105,brake_resistance=1/300,push=2,resistance_present=True)
-bird1=Bird((100,50),(50,27),3.5,world,(pygame.K_SPACE,0))
+world=Physicsworld(gravity=1000,elasticity=1,resistance=0.004,normal=105,brake_resistance=1/300,push=2,resistance_present=True)
+bird1=Bird((100,200),(50,27),3.8,world,(pygame.K_SPACE,0))
 game_objects=[]
 game_objects.append(bird1)
-SCREEN = Screen((0,0),(600,600),game_objects,400)
+SCREEN = Screen((0,0),(600,600),game_objects,300)
+bird1.set_screen(SCREEN)
 prev_time=pygame.time.get_ticks()
+Font = pygame.font.SysFont("comicsans",30,True)
 while run:
-    clock.tick(200)
+    clock.tick(64)
     time=pygame.time.get_ticks()
     dt = time-prev_time
     dt/=1000.0
@@ -175,11 +220,9 @@ while run:
     for obj in SCREEN.objects:
         obj.general_operations_manager(keys,dt)
     SCREEN.general_operations_manager(keys,dt)
-    if pygame.time.get_ticks() >= start+1000 and game_started:
-        print("here")
+    if pygame.time.get_ticks() >= start+1000 and bird1.game_started:
         pipe = Pipe(SCREEN,650,bird1)
         SCREEN.objects.append(pipe)
         start=pygame.time.get_ticks()
     SCREEN.redraw()
 pygame.quit()
-
